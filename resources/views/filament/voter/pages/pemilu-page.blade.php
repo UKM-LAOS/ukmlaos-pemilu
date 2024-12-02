c66c7ea15179b9eb07695a9063880fb71ff801c2
ChristYoga123 <christianuswibisono@gmail.com>
2024-12-02 12:47:41 +0700

    fix: merapikan

--- resources/views/filament/voter/pages/pemilu-page.blade.php
+++ resources/views/filament/voter/pages/pemilu-page.blade.php
@@ -1,12 +1,14 @@
 <x-filament-panels::page>
-    <strong>Visi & Misi</strong>
-    {{ $this->infolist }}
-    <strong>Form Voting</strong>
-    <x-filament-panels::form wire:submit="save"> 
-        {{ $this->form }}
- 
-        <x-filament-panels::form.actions 
-            :actions="$this->getFormActions()"
-        /> 
-    </x-filament-panels::form>
+    <div class="flex justify-center flex-col items-center">
+        <strong style="margin-bottom: 10px">Visi & Misi</strong>
+        {{ $this->infolist }}
+        <strong style="margin: 10px 0 10px 0">Form Voting</strong>
+        <x-filament-panels::form wire:submit="save">
+            {{ $this->form }}
+
+            <div class="flex justify-center">
+                <x-filament-panels::form.actions :actions="$this->getFormActions()" />
+            </div>
+        </x-filament-panels::form>
+    </div>
 </x-filament-panels::page>
--- resources/views/livewire/voter/pages/pemilu-page/components/visi-misi-component.blade.php
+++ resources/views/livewire/voter/pages/pemilu-page/components/visi-misi-component.blade.php
@@ -1,12 +1,13 @@
 <div>
     <div class="flex items-center flex-col">
+        {{-- {{ $infolist }} --}}
         <strong>Visi:</strong>
         <div class="text-center">
             {!! $caketum->visi !!}
         </div>
         <br />
         <strong>Misi:</strong>
-        <div class="text-center">
+        <div class="text-justify">
             {!! $caketum->misi !!}
         </div>
     </div>
